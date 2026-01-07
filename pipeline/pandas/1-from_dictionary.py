08bd5d7e16c1822793c430c6874c21792c288b39
sananasgrv <sanan.asgarov2023@khazar.org>
2026-01-07 18:07:18 +0400

    Commit

--- pipeline/pandas/1-from_dictionary.py
+++ pipeline/pandas/1-from_dictionary.py
@@ -8,4 +8,4 @@ df = pd.DataFrame(
             "First": pd.Series([0.0, 0.5, 1.0, 1.5], dtype=float),
             "Second": pd.Series(["one", "two", "three", "four"], dtype=str),
         },
-index=list("AB"))
+index=list("ABCD"))
