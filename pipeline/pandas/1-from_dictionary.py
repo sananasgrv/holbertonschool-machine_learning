b5f19c3d53c0620ab5f819cc87032bd4af621e50
sananasgrv <sanan.asgarov2023@khazar.org>
2026-01-07 18:45:11 +0400

    commit

--- pipeline/pandas/1-from_dictionary.py
+++ pipeline/pandas/1-from_dictionary.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 df = pd.DataFrame(
 {
-        "First": ([0.0, 0.5, 1.0, 1.5]),
-        "Second": (["one", "two", "three", "four"]),
+    "First": ([0.0, 0.5, 1.0, 1.5]),
+    "Second": (["one", "two", "three", "four"]),
     },
 index=list("ABCD"))
