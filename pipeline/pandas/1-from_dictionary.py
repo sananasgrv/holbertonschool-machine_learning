f9751412f1bc4ed4c5c6dec77640b9de5ad5c7fe
sananasgrv <sanan.asgarov2023@khazar.org>
2026-01-07 17:55:34 +0400

    Commit

--- pipeline/pandas/1-from_dictionary.py
+++ pipeline/pandas/1-from_dictionary.py
@@ -4,6 +4,6 @@ import pandas as pd
 
 
 df = pd.DataFrame({
-    "First": pd.series([0.0, 0.5, 1.0, 1.5]),
-    "Second": pd.Series(['one', 'two', 'three', 'four']),
+    "First": pd.seri([0.0, 0.5, 1.0, 1.5], dtype=float),
+    "Second": pd.Series(['one', 'two', 'three', 'four'], dtype=str),
 }, index=list("AB"))
