18a5767d1f27ed233a4e4768e1202d6a5e167b61
sananasgrv <sanan.asgarov2023@khazar.org>
2026-01-07 17:52:27 +0400

    Commit

---
+++ pipeline/pandas/1-from_dictionary.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Documented"""
+import pandas as pd
+
+
+df = pd.DataFrame({
+    "First": pd.series([0.0, 0.5, 1.0, 1.5])
+    "Second": pd.Series(['one', 'two', 'three', 'four'])
+}, index=list("AB"))
