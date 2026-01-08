08d275a5897803159ee82f4a16a2ca5720feaebf
sananasgrv <sanan.asgarov2023@khazar.org>
2026-01-08 15:54:05 +0400

    commit

--- pipeline/pandas/4-array.py
+++ pipeline/pandas/4-array.py
@@ -4,7 +4,5 @@
 
 def array(df):
     """Documented"""
-    new_df = df.column(["High", "Close"])
-    new_df = new_df.tail(10)
-    new_df.to_numpy()
-    return new_df
+    df.loc[:10, ["High", "Close"]]
+    return df
