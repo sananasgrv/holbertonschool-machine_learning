93356aadd03aa35442806e661249992f3a433917
sananasgrv <sanan.asgarov2023@khazar.org>
2026-01-08 20:19:55 +0400

    commit

--- pipeline/pandas/11-concat.py
+++ pipeline/pandas/11-concat.py
@@ -6,4 +6,5 @@ index = __import__('10-index').index
 
 def concat(df1, df2):
     """Documented"""
+    pd.merge(df1, df2, )
     return df1,df2
--- pipeline/pandas/9-fill.py
+++ pipeline/pandas/9-fill.py
@@ -6,8 +6,7 @@ def fill(df):
     """Documented"""
     df = df.drop(columns=['Weighted_Price'])
     df["Close"] = df["Close"].ffill()
-    df[["High", "Low", "Open"]] = (
-        df[["High", "Low", "Open"]].fillna(df["Close"]).ffill())
+    df[["High", "Low", "Open"]] = df[["High", "Low", "Open"]].fillna(df["Close"], axis=0)
     df[["Volume_(BTC)", "Volume_(Currency)"]] = (
         df[["Volume_(BTC)", "Volume_(Currency)"]].fillna(value=0))
     return df
