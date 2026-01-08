f5c72d77c79695f0acd70416cd7c4fdf021e500c
sananasgrv <sanan.asgarov2023@khazar.org>
2026-01-09 01:16:13 +0400

    commit

--- pipeline/pandas/12-hierarchy.py
+++ pipeline/pandas/12-hierarchy.py
@@ -6,4 +6,9 @@ index = __import__('10-index').index
 
 def hierarchy(df1, df2):
     """Hierarchical DataFrame"""
-    return "salam"
+    df1 = index(df1)
+    df2 = index(df2)
+    df = pd.concat([df2, df1], keys=["bitstamp", "coinbase"])
+    df = df.sort_index()
+    col = list(df.columns)
+    return df
