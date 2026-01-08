0892e863e82a3037e781a41bf3181c927f7704c6
sananasgrv <sanan.asgarov2023@khazar.org>
2026-01-09 00:47:46 +0400

    commit

--- pipeline/pandas/11-concat.py
+++ pipeline/pandas/11-concat.py
@@ -6,8 +6,8 @@ index = __import__('10-index').index
 
 def concat(df1, df2):
     """Documented"""
-    index(df1)
-    index(df2)
-    df2 = df2.loc[:1417411920, :]
+    df1 = index(df1)
+    df2 = index(df2)
+    df2 = df2.loc[:1417411920]
     df = pd.concat([df2, df1], keys=["bitstamp", "coinbase"])
     return df
