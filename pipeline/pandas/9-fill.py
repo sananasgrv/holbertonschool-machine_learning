de26d63417c04851cad12a6cd4f90d7277c75fc6
sananasgrv <sanan.asgarov2023@khazar.org>
2026-01-08 20:22:30 +0400

    commit

--- pipeline/pandas/9-fill.py
+++ pipeline/pandas/9-fill.py
@@ -6,7 +6,8 @@ def fill(df):
     """Documented"""
     df = df.drop(columns=['Weighted_Price'])
     df["Close"] = df["Close"].ffill()
-    df[["High", "Low", "Open"]] = (df[["High", "Low", "Open"]].fillna(df["Close"]).ffill())
+    df[["High", "Low", "Open"]] = (
+        df[["High", "Low", "Open"]].fillna(df["Close"]).ffill())
     df[["Volume_(BTC)", "Volume_(Currency)"]] = (
         df[["Volume_(BTC)", "Volume_(Currency)"]].fillna(value=0))
     return df
