b95cb37532ccaff183398bbbbb6075576da417f2
sananasgrv <sanan.asgarov2023@khazar.org>
2026-01-09 01:36:27 +0400

    commit

--- pipeline/pandas/12-hierarchy.py
+++ pipeline/pandas/12-hierarchy.py
@@ -8,6 +8,8 @@ def hierarchy(df1, df2):
     """Hierarchical DataFrame"""
     df1 = index(df1)
     df2 = index(df2)
-    df = pd.MultiIndex.from_product([df2, df1], names=["bitstamp", "coinbase"])
+    df = pd.concat([df2, df1], keys=["bitstamp", "coinbase"])
+    df = df.swaplevel(0, 1)
     df = df.sort_index()
+    df = df.loc[1417411980:1417417980]
     return df
