888cebc6d304f716561e0347a343cdce7f8ecfb6
sananasgrv <sanan.asgarov2023@khazar.org>
2026-01-08 15:43:52 +0400

    commit

--- pipeline/pandas/3-rename.py
+++ pipeline/pandas/3-rename.py
@@ -7,5 +7,5 @@ def rename(df):
     """Documented"""
     new_df = df.loc[:, ["Timestamp", "Close"]]
     new_df = new_df.rename(columns={"Timestamp": "Datetime"})
-    new_df = new_df(["Datetime"]).timestamp()
+    new_df = new_df.to_datetime(["Datetime"], unit='s')
     return new_df
