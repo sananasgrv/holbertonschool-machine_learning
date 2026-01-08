c962855aeb5ae87da7d9e7ade32ba85c550171d8
sananasgrv <sanan.asgarov2023@khazar.org>
2026-01-08 16:24:00 +0400

    commit

--- pipeline/pandas/5-slice.py
+++ pipeline/pandas/5-slice.py
@@ -4,5 +4,5 @@
 
 def slice(df):
     """Documented"""
-    new_df = df.loc[:60, ["High", "Low", "Close", "Volume_BTC"]]
+    new_df = df.loc[0:60, ["High", "Low", "Close", "Volume_BTC"]]
     return new_df
