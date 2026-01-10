732bca006782222c6c790e33534bab9fbd347407
sananasgrv <sanan.asgarov2023@khazar.org>
2026-01-10 22:18:23 +0400

    Commit

---
+++ math/plotting/1-scatter.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Documented file"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def scatter():
+    """Documented function"""
+    mean = [69, 0]
+    cov = [[15, 8], [8, 15]]
+    np.random.seed(5)
+    x, y = np.random.multivariate_normal(mean, cov, 2000).T
+    y += 180
+    plt.figure(figsize=(6.4, 4.8))
+    plt.scatter(x, y)
+    plt.title("Men's Height vs Weight")
+    plt.xlabel("Height (in)")
+    plt.ylabel("Weight (lbs)")
+    return plt.show()
