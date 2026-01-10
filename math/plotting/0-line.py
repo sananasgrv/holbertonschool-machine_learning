e521d92534e500ada730c2c19e2fc3d473683a2a
sananasgrv <sanan.asgarov2023@khazar.org>
2026-01-10 22:10:12 +0400

    Commit

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -2,10 +2,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def line():
     """Documented line function"""
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
-    plt.plot(y,color="red")
-    plt.xlim([0,10])
+    plt.plot(y, color="red")
+    plt.xlim([0, 10])
     return plt.show()
