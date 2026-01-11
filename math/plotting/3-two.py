3baaa751929bcd6eeed9781c51d3bb24347b5d33
sananasgrv <sanan.asgarov2023@khazar.org>
2026-01-11 16:08:01 +0400

    Commit

---
+++ math/plotting/3-two.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""
+Complete the following source code
+    to plot x ↦ y1 and x ↦ y2 as line graphs:
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+def two():
+    """
+    The x-axis should be labeled Time (years)
+    The y-axis should be labeled Fraction Remaining
+    The title should be Exponential Decay of Radioactive Elements
+    The x-axis should range from 0 to 20,000
+    The y-axis should range from 0 to 1
+    x ↦ y1 should be plotted with a dashed red line
+    x ↦ y2 should be plotted with a solid green line
+    A legend labeling x ↦ y1 as C-14 and x ↦ y2 as Ra-226
+        should be placed in the upper right hand corner of the plot
+    """
+    x = np.arange(0, 21000, 1000)
+    r = np.log(0.5)
+    t1 = 5730
+    t2 = 1600
+    y1 = np.exp((r / t1) * x)
+    y2 = np.exp((r / t2) * x)
+    plt.figure(figsize=(6.4, 4.8))
+    plt.title("Exponential Decay of C-14")
+    plt.xlabel("Time (years)")
+    plt.ylabel("Fraction Remaining")
+    plt.xlim(0, 20000)
+    plt.ylim(0, 1)
+    plt.plot(x, y1, color="red", ls="dashed", label="C-14")
+    plt.plot(x, y2, color="green", label="Ra-226")
+    plt.legend(["Ca-14", "Ra-226"], loc="upper right")
+    return plt.show()
+
+print(two())
