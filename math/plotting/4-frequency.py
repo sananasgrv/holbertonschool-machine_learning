e00015c5be9458ece91327f16f4ce87816687f82
sananasgrv <sanan.asgarov2023@khazar.org>
2026-01-11 16:36:09 +0400

    Commit

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -6,6 +6,7 @@ Complete the following source code to plot
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def frequency():
     """
     The x-axis should be labeled Grades
@@ -15,7 +16,7 @@ def frequency():
     The bars should be outlined in black
     """
     np.random.seed(5)
-    student_grades = np.random.normal(68, 15, 50)
+    student_grades=np.random.normal(68, 15, 50)
     bins1=np.arange(0, 101, 10)
     plt.figure(figsize=(6.4, 4.8))
     plt.title("Project A")
