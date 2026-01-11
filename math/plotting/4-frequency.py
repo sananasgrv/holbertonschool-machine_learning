350d48e0585ea68dc21f0cd6698c37c133169df4
sananasgrv <sanan.asgarov2023@khazar.org>
2026-01-11 16:36:40 +0400

    Commit

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -16,8 +16,8 @@ def frequency():
     The bars should be outlined in black
     """
     np.random.seed(5)
-    student_grades=np.random.normal(68, 15, 50)
-    bins1=np.arange(0, 101, 10)
+    student_grades = np.random.normal(68, 15, 50)
+    bins1 = np.arange(0, 101, 10)
     plt.figure(figsize=(6.4, 4.8))
     plt.title("Project A")
     plt.xlabel("Grades")
