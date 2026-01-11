58b77ba12e021516b2dad933131e24d1930dfcf5
sananasgrv <sanan.asgarov2023@khazar.org>
2026-01-11 16:32:13 +0400

    Commit

---
+++ math/plotting/4-frequency.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+Complete the following source code to plot
+    a histogram of student scores for a project:
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+def frequency():
+    """
+    The x-axis should be labeled Grades
+    The y-axis should be labeled Number of Students
+    The x-axis should have bins every 10 units
+    The title should be Project A
+    The bars should be outlined in black
+    """
+    np.random.seed(5)
+    student_grades = np.random.normal(68, 15, 50)
+    bins1=np.arange(0, 100, 10)
+    plt.figure(figsize=(6.4, 4.8))
+    plt.title("Project A")
+    plt.xlabel("Grades")
+    plt.ylabel("Number of Students")
+    plt.hist(student_grades, bins=bins1, edgecolor="black")
+    plt.xticks(bins1)
+
+    return plt.show()
