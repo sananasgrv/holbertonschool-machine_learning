6e73abf49e473a24a09c65db8c02881ec30602c4
sananasgrv <sanan.asgarov2023@khazar.org>
2026-01-14 17:07:45 +0400

    Commit

---
+++ math/calculus/10-matisse.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+Write a function def poly_derivative(poly): that calculates the derivative of a polynomial:
+"""
+
+
+def poly_derivative(poly):
+    """
+    poly is a list of coefficients representing a polynomial
+
+    the index of the list represents the power of x that the coefficient belongs to
+    Example: if <img src=“https://latex.codecogs.com/gif.latex?f(x)&space;=&space;x^3&space;&plus;&space;3x&space;&plus;5” title=“f(x) = x^3 + 3x +5” />, poly is equal to [5, 3, 0, 1]
+    If poly is not valid, return None
+    If the derivative is 0, return [0]
+    Return a new list of coefficients representing the derivative of the polynomial
+    """
+    if poly is None :
+        return None
+    else:
+        result = []
+        for i in range(len(poly)):
+            if poly[i] * i != 0:
+                result.append(poly[i] * i)
+        return result
