6d0502d6d936f22cac04e818f662e2f7821c13df
sananasgrv <sanan.asgarov2023@khazar.org>
2026-01-14 17:53:45 +0400

    Commit

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -5,14 +5,15 @@
 def poly_integral(poly, C=0):
     if type(poly) is not list or len(poly) == 0:
         return None
-    elif type(C) is int:
-        if poly == 0:
-            return C
-        else:
-            result = [C]
-            for i in range(len(poly)):
-                result.append(poly[i] / (i+1))
-                result[i] = int(result[i]) if result[i] % 1 == 0 else result[i]
-        return result
-    else:
+    if type(C) is not int:
         return None
+
+    result = [C]
+
+    for i in range(len(poly)):
+        val = poly[i] / (i + 1)
+        if val.is_integer():
+            val = int(val)
+        result.append(val)
+
+    return result
