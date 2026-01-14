849eb6ad8977641d01c4109656090780d465b497
sananasgrv <sanan.asgarov2023@khazar.org>
2026-01-14 17:20:36 +0400

    Commit

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -1,20 +1,24 @@
 #!/usr/bin/env python3
 """
-Write a function def poly_derivative(poly): that calculates the derivative of a polynomial:
+Write a function def poly_derivative(poly):
+    that calculates the derivative of a polynomial:
 """
 
 
 def poly_derivative(poly):
     """
     poly is a list of coefficients representing a polynomial
-
-    the index of the list represents the power of x that the coefficient belongs to
-    Example: if <img src=“https://latex.codecogs.com/gif.latex?f(x)&space;=&space;x^3&space;&plus;&space;3x&space;&plus;5” title=“f(x) = x^3 + 3x +5” />, poly is equal to [5, 3, 0, 1]
+    the index of the list represents the
+        power of x that the coefficient belongs to
+    Example: if <img src=“https://latex.codecogs.com/gif.latex
+        ?f(x)&space;=&space;x^3&space;&plus;&space;3x&space;&plus;5”
+            title=“f(x) = x^3 + 3x +5” />, poly is equal to [5, 3, 0, 1]
     If poly is not valid, return None
     If the derivative is 0, return [0]
-    Return a new list of coefficients representing the derivative of the polynomial
+    Return a new list of coefficients
+        representing the derivative of the polynomial
     """
-    if type(poly) is not list or poly == [] :
+    if type(poly) is not list or poly == []:
         return None
     elif len(poly) == 1:
         return [0]
@@ -22,6 +26,6 @@ def poly_derivative(poly):
         result = []
         for i in range(len(poly)):
                 result.append(poly[i] * i)
-        if result[0] == 0 :
+        if result[0] == 0:
             result.pop(0)
         return result
