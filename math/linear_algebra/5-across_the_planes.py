f748a09f24a5bd949a0e4533dfe9fd35f39e785b
sananasgrv <sanan.asgarov2023@khazar.org>
2026-01-26 19:33:27 +0400

    Commit

---
+++ math/linear_algebra/5-across_the_planes.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Documented"""
+from unittest import result
+
+
+def add_matrices2D(mat1, mat2):
+    if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
+        return None
+    result = []
+    for i in range(len(mat1[0])):
+        result.append([mat1[j][i] + mat2[j][i]for j in range(len(mat1))])
+    return result
