76816b611cd6632b6ce13163f9498b718230ff31
sananasgrv <sanan.asgarov2023@khazar.org>
2026-01-26 22:24:13 +0400

    Commit

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Documented"""
+
+def cat_arrays(arr1, arr2):
+    """Documented"""
+    result = arr1.copy()
+    for i in arr2:
+        result.append(i)
+    return result
