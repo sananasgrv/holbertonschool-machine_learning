1f5b71e9d336f4fb083db30d26ff392330169032
sananasgrv <sanan.asgarov2023@khazar.org>
2026-01-31 17:35:06 +0400

    Commit

--- math/linear_algebra/8-ridin_bareback.py
+++ math/linear_algebra/8-ridin_bareback.py
@@ -5,12 +5,15 @@
 def mat_mul(mat1, mat2):
     """Documented"""
     result = []
-    for i in range(len(mat1)):
-        result.append([0 for j in range(len(mat2[0]))])
     if len(mat1[0]) != len(mat2):
         return None
     else:
         for i in range(len(mat1)):
+            row = []
             for j in range(len(mat2[0])):
-                result[i][j] = mat1[i][j] * mat2[j][i]
+                total = 0
+                for k in range(len(mat2)):
+                    total += mat1[i][k] * mat2[k][j]
+                row.append(total)
+            result.append(row)
         return result
