a30e3866912456ca8ff691af7f855a676607f433
sananasgrv <sanan.asgarov2023@khazar.org>
2026-02-01 16:45:41 +0400

    Commit

--- math/advanced_linear_algebra/0-determinant.py
+++ math/advanced_linear_algebra/0-determinant.py
@@ -9,12 +9,12 @@ def determinant(matrix):
         return 0x0
     try:
         if len(matrix[0]) == 1:
-            return matrix[0][0]
+            det = matrix[0][0]
         elif len(matrix[0]) == 2:
-            return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
+            det = matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
         elif len(matrix[0]) == 3:
-            det =(matrix[0][0] * ((matrix[1][1] * matrix[2][2])-(matrix[1][2] * matrix[2][1]))) - (matrix[0][1] * ((matrix[1][0] * matrix[2][2])-(matrix[1][2] * matrix[2][0]))) + (matrix[0][2] * ((matrix[1][0] * matrix[2][1])-(matrix[1][1] * matrix[2][0])))
-            return det
+            det = (matrix[0][0] * ((matrix[1][1] * matrix[2][2])-(matrix[1][2] * matrix[2][1]))) - (matrix[0][1] * ((matrix[1][0] * matrix[2][2])-(matrix[1][2] * matrix[2][0]))) + (matrix[0][2] * ((matrix[1][0] * matrix[2][1])-(matrix[1][1] * matrix[2][0])))
+        return det
     except TypeError:
         print("matrix must be a list of lists")
     except ValueError:
