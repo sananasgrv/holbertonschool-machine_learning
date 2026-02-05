a6d7e44b41c2f20712ddddf2e824fa36a58a3a94
Sanan <sanan.asgarov2023@khazar.org>
2026-02-05 17:27:04 +0400

    Fix error handling for non-square matrices

--- math/advanced_linear_algebra/0-determinant.py
+++ math/advanced_linear_algebra/0-determinant.py
@@ -11,10 +11,10 @@ def determinant(matrix):
         if type(i) != list:
             raise TypeError("matrix must be a list of lists")
         if len(i) != len(matrix):
-            raise TypeError("matrix must be a list of lists")
+            raise ValueError("matrix must be a square matrix")
     if len(matrix) == 1:
         return matrix[0][0]
-    if len(matrix[0]) == 2:
+    if len(matrix) == 2:
         return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
     else:
         for col in range(len(matrix)):
