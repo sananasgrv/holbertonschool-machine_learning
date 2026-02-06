b904902d21ca0dbe140f3ce6937ee83a919e3fb9
sananasgrv <sanan.asgarov2023@khazar.org>
2026-02-07 01:43:25 +0400

    Commit

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -4,34 +4,36 @@
 
 class Poisson:
     """Poisson class"""
+
+    e = 2.7182818285
+
     def __init__(self, data=None, lambtha=1.):
         self.data = data
         if data is None:
             if lambtha <= 0:
                 raise ValueError("lambtha must be a positive value")
-            else:
-                self.lambtha = float(lambtha)
+            self.lambtha = float(lambtha)
         else:
             if not isinstance(data, list):
                 raise TypeError("data must be a list")
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
-            self.lambtha = sum(data)/len(data)
+            self.lambtha = sum(data) / len(data)
 
-    e = 2.7182818285
     def pmf(self, k):
         """Probability density function"""
         if not isinstance(k, int):
             k = int(k)
         if k < 0:
             return 0
-        e = 2.7182818285
-        first_nom = pow(e, -self.lambtha)
+
+        first_nom = pow(self.e, -self.lambtha)
         second_nom = pow(self.lambtha, k)
 
         denominator = 1
-        for i in range(1, k+1):
+        for i in range(1, k + 1):
             denominator *= i
+
         return (first_nom * second_nom) / denominator
 
     def cdf(self, k):
@@ -40,8 +42,9 @@ class Poisson:
             k = int(k)
         if k < 0:
             return 0
-        e = 2.7182818285
+
         cdf_value = 0
-        for i in range(k+1):
+        for i in range(k + 1):
             cdf_value += self.pmf(i)
+
         return cdf_value
