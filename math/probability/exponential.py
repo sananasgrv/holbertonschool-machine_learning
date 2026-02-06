7d0e21af42692e0effe7212b9bbc66c7d5ce21c9
sananasgrv <sanan.asgarov2023@khazar.org>
2026-02-07 02:05:44 +0400

    Commit

--- math/probability/exponential.py
+++ math/probability/exponential.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python3
-"""Exponential distribution class"""
-
-
-import math
+"""Documented"""
 
 
 class Exponential:
-    """Exponential distribution"""
-
+    """docstring for Exponential"""
+    e = 2.7182818285
     def __init__(self, data=None, lambtha=1.):
         if data is None:
             if lambtha <= 0:
@@ -18,10 +15,11 @@ class Exponential:
                 raise TypeError("data must be a list")
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
-            self.lambtha = 1 / (sum(data) / len(data))
+            self.lambtha = 1/(sum(data)/len(data))
 
     def pdf(self, x):
-        """Probability density function at x"""
-        if x < 0:
+        """Documented"""
+        if x <= 0:
             return 0
-        return self.lambtha * math.exp(-self.lambtha * x)
+        pdf = self.lambtha * pow(self.e, -self.lambtha * x)
+        return pdf
