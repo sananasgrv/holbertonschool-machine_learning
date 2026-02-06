66edd2ff45774c6f1de79e59b2d4cd4934528cae
sananasgrv <sanan.asgarov2023@khazar.org>
2026-02-07 01:52:42 +0400

    Commit

---
+++ math/probability/exponential.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Documented"""
+import numpy as np
+
+
+class Exponential:
+    """docstring for Exponential"""
+    def __init__(self, data=None, lambtha=1.):
+        if data is None:
+            if lambtha < 0:
+                raise ValueError("lambtha must be a positive value")
+            self.lambtha = float(lambtha)
+        else:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            self.lambtha = sum(data)/len(data)
