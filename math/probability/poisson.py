d7f0ce9d5abd16f1144eb1b11d69d9e6cc52baac
sananasgrv <sanan.asgarov2023@khazar.org>
2026-02-07 01:09:11 +0400

    Commit

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -12,8 +12,8 @@ class Poisson:
             else:
                 self.lambtha = float(lambtha)
         else:
-            self.lambtha = sum(data)/len(data)
             if not isinstance(data, list):
                 raise TypeError("data must be a list")
-            if len(data) <= 2:
+            if len(data) < 2:
                 raise ValueError("data must contain multiple values")
+            self.lambtha = sum(data)/len(data)
