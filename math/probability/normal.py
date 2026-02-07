31cd7f9f0e321a597852b2b85d721a23301d3e33
sananasgrv <sanan.asgarov2023@khazar.org>
2026-02-07 13:00:12 +0400

    Commit

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -7,14 +7,15 @@ class Normal:
 
     def __init__(self, data=None, mean=0., stddev=1.):
         if data is None:
-            self.mean = float(mean)
-            if stddev < 0:
+            if stddev <= 0:
                 raise ValueError("stddev must be a positive value")
+            self.mean = float(mean)
             self.stddev = float(stddev)
         else:
             if isinstance(data, list):
                 raise TypeError("data must be a list")
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
-            mean = sum(data) / len(data)
-            stddev = sum((data[i]-self.mean)**2 for i in range(len(data))) / len(data)
+            self.mean = sum(data) / len(data)
+            self.stddev = (sum((data[i]-self.mean)**2 for i in range(len(data))) / len(data) ** 0.5)
+
