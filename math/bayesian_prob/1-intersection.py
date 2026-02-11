7440abe16eeedd75b329d300c1dc4a628cc204f8
sananasgrv <sanan.asgarov2023@khazar.org>
2026-02-11 17:06:08 +0400

    Commit

--- math/bayesian_prob/1-intersection.py
+++ math/bayesian_prob/1-intersection.py
@@ -7,25 +7,23 @@ def intersection(x, n, P, Pr):
     """ that calculates the intersection of
     obtaining this data
      with the various hypothetical probabilities"""
-    if not isinstance(n, int) or n <= 0:
+    if (type(n) is not int) or (n <= 0):
         raise ValueError("n must be a positive integer")
-    if not isinstance(x, int) or x < 0:
-        raise ValueError("x must be an integer that"
-                         " is greater than or equal to 0")
+    if (type(x) is not int) or (x < 0):
+        raise ValueError("x must be an integer that is greater than or equal "
+                         "to 0")
     if x > n:
         raise ValueError("x cannot be greater than n")
 
-    if not isinstance(P, np.ndarray):
+    if (type(P) is not np.ndarray) or (len(P.shape) != 1):
         raise TypeError("P must be a 1D numpy.ndarray")
-    if not isinstance(Pr, np.ndarray) or Pr.shape != P.shape:
+    if (type(Pr) is not np.ndarray) or (P.shape != Pr.shape):
         raise TypeError("Pr must be a numpy.ndarray with the same shape as P")
 
-    if P.ndim != 1:
-        raise TypeError("P must be a 1D numpy.ndarray")
-    if Pr.ndim != 1:
-        raise TypeError("Pr must be a 1D numpy.ndarray")
-
-    if np.any(P < 0) or np.any(P > 1):
-        raise ValueError("All values in P must be in the range [0, 1]")
-    if np.any(Pr < 0) or np.any(Pr > 1):
-        raise ValueError("All values in Pr must be in the range [0, 1]")
+    for p, pr in zip(P, Pr):
+        if not (p >= 0 and p <= 1):
+            raise ValueError("All values in P must be in the range [0, "
+                             "1]")
+        if not (pr >= 0 and pr <= 1):
+            raise ValueError("All values in Pr must be in the range [0, "
+                             "1]")
