8f683e64a2f6e20922d70ae7ae2b0bca71cf12fc
sananasgrv <sanan.asgarov2023@khazar.org>
2026-02-11 14:38:17 +0400

    Commit

---
+++ math/bayesian_prob/0-likelihood.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Documented"""
+import numpy
+
+
+def likelihood(x, n, P):
+    """Calculate the likelihood of a given probability distribution"""
+    if n < 0 or not isinstance(n, int):
+        raise ValueError("n must be a positive integer")
+    if not isinstance(x, int) or x < 0:
+        raise ValueError("x must be an integer that is greater than or equal to 0")
+    if x > n:
+        raise ValueError("x cannot be greater than n")
+    if not  isinstance(x, numpy.ndarray) or P.ndim != 1:
+        raise ValueError("P must be a 1D numpy.ndarray")
+    for i in P:
+        if i > 1 or i<0:
+            raise ValueError("All values in P must be in the range [0, 1]")
+
