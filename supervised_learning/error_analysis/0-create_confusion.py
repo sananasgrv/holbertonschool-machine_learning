7a85256f3f6894fde403b317bc6e9f4719091114
Sanan <sanan.asgarov2023@khazar.org>
2026-02-18 15:07:29 +0400

    Commit

--- supervised_learning/error_analysis/0-create_confusion.py
+++ supervised_learning/error_analysis/0-create_confusion.py
@@ -5,13 +5,4 @@ import numpy as np
 
 def create_confusion_matrix(labels, logits):
     """Documented"""
-    m, classes = labels.shape
-    true_classes = np.argmax(labels, axis=1)
-    pred_classes = np.argmax(logits, axis=1)
-
-    conf = np.zeros(shape=(classes, classes))
-
-    for i in range(classes):
-        conf[true_classes[i], pred_classes[i]] = true_classes[i]
-
-    return conf
+    return np.matmul(labels.T, logits)
