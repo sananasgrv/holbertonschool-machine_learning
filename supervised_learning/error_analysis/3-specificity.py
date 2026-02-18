327507abe9a153c56ce8ec3e849e7563938ce2e5
Sanan <sanan.asgarov2023@khazar.org>
2026-02-18 16:28:34 +0400

    Commit

--- supervised_learning/error_analysis/3-specificity.py
+++ supervised_learning/error_analysis/3-specificity.py
@@ -9,6 +9,6 @@ def specificity(confusion):
     """
     TP = np.diagonal(confusion)
     FP = np.sum(confusion, axis=0) - TP
-    FN = np.sum(confusion, axis=1) -TP
+    FN = np.sum(confusion, axis=1) - TP
     TN = np.sum(confusion) - (TP+FP+FN)
     return TN / (TN+FP)
