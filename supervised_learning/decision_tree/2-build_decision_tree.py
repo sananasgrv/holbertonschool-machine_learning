17317f6b7b4f0e851e3013a89892c216f3b37f71
Sanan <sanan.asgarov2023@khazar.org>
2026-02-21 22:22:47 +0400

    Commit

--- supervised_learning/decision_tree/2-build_decision_tree.py
+++ supervised_learning/decision_tree/2-build_decision_tree.py
@@ -31,14 +31,14 @@ class Node:
 
     def left_child_add_prefix(self, text):
         lines = text.split("\n")
-        new_text = "    +--->" + lines[0] + "\n"
+        new_text = "    +---> " + lines[0] + "\n"
         for x in lines[1:]:
             new_text += ("    |  " + x) + "\n"
         return (new_text)
 
     def right_child_add_prefix(self, text):
         lines = text.split("\n")
-        new_text = "    +--->" + lines[0] + "\n"
+        new_text = "    +---> " + lines[0] + "\n"
         for x in lines[1:]:
             new_text += "       " + x + "\n"
         return new_text
