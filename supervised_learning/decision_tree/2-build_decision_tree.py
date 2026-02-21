d8e907f57077c073027c177695db0b7b4ee94b4c
Sanan <sanan.asgarov2023@khazar.org>
2026-02-21 22:43:57 +0400

    Commit

--- supervised_learning/decision_tree/2-build_decision_tree.py
+++ supervised_learning/decision_tree/2-build_decision_tree.py
@@ -1,62 +1,74 @@
 #!/usr/bin/env python3
 """Documented"""
+
 import numpy as np
 
 
 class Node:
     """Documented"""
+
     def __init__(self, feature=None, threshold=None, left_child=None,
                  right_child=None, is_root=False, depth=0):
+        """Documented"""
         self.feature = feature
         self.threshold = threshold
         self.left_child = left_child
         self.right_child = right_child
         self.is_leaf = False
         self.is_root = is_root
+        self.sub_population = None
         self.depth = depth
 
     def max_depth_below(self):
         """Documented"""
-        left_depth = self.left_child.max_depth_below() if self.left_child else self.depth
-        right_depth = self.right_child.max_depth_below() if self.right_child else self.depth
-        return max(left_depth, right_depth)
+        return max(self.left_child.max_depth_below(),
+                   self.right_child.max_depth_below())
 
     def count_nodes_below(self, only_leaves=False):
         """Documented"""
-        left_count = self.left_child.count_nodes_below(only_leaves) if self.left_child else 0
-        right_count = self.right_child.count_nodes_below(only_leaves) if self.right_child else 0
-        if only_leaves:
-            return left_count + right_count
-        return 1 + left_count + right_count
+        return (self.left_child.count_nodes_below(only_leaves=only_leaves) +
+                self.right_child.count_nodes_below(only_leaves=only_leaves) +
+                (0 if only_leaves else 1))
 
     def left_child_add_prefix(self, text):
+        """Documented"""
         lines = text.split("\n")
-        new_text = "    +--->" + lines[0] + "\n"
+        new_text = "    +---> " + lines[0] + "\n"
         for x in lines[1:]:
-            new_text += ("    |  " + x) + "\n"
-        return (new_text)
+            if x:
+                new_text += ("    |  " + x) + "\n"
+        return new_text
 
     def right_child_add_prefix(self, text):
+        """Documented"""
         lines = text.split("\n")
-        new_text = "    +--->" + lines[0] + "\n"
+        new_text = "    +---> " + lines[0] + "\n"
         for x in lines[1:]:
-            new_text += ("       " + x) + "\n"
-        return (new_text)
+            if x:
+                new_text += ("       " + x) + "\n"
+        return new_text
 
     def __str__(self):
+        """Documented"""
         if self.is_root:
-            text = f"root [feature={self.feature}, threshold={self.threshold}]\n"
+            out = (f"root [feature={self.feature}, "
+                   f"threshold={self.threshold}]\n")
         else:
-            text = f"node [feature={self.feature}, threshold={self.threshold}]\n"
+            out = (f"node [feature={self.feature}, "
+                   f"threshold={self.threshold}]\n")
 
-        text += self.left_child_add_prefix(str(self.left_child)).rsplit("\n") if self.left_child else 0
-        text += self.right_child_add_prefix(str(self.right_child)).rsplit("\n") if self.right_child else 0
+        if self.left_child:
+            out += self.left_child_add_prefix(str(self.left_child))
+        if self.right_child:
+            out += self.right_child_add_prefix(str(self.right_child))
+        return out
 
-        return text
 
 class Leaf(Node):
     """Documented"""
+
     def __init__(self, value, depth=None):
+        """Documented"""
         super().__init__()
         self.value = value
         self.is_leaf = True
@@ -72,13 +84,15 @@ class Leaf(Node):
 
     def __str__(self):
         """Documented"""
-        return f"leaf [value={self.value}]"
+        return "leaf [value={}]".format(self.value)
 
 
-class Decision_Tree:
+class Decision_Tree():
     """Documented"""
-    def __init__(self, max_depth=10, min_pop=1,
-                 seed=0, split_criterion="random", root=None):
+
+    def __init__(self, max_depth=10, min_pop=1, seed=0,
+                 split_criterion="random", root=None):
+        """Documented"""
         self.rng = np.random.default_rng(seed)
         if root:
             self.root = root
@@ -89,6 +103,7 @@ class Decision_Tree:
         self.max_depth = max_depth
         self.min_pop = min_pop
         self.split_criterion = split_criterion
+        self.predict = None
 
     def depth(self):
         """Documented"""
