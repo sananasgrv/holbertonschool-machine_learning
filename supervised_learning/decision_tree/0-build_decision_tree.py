d5cec400cb0b541381548bfa238daa66ebac1825
Sanan <sanan.asgarov2023@khazar.org>
2026-02-22 23:50:17 +0400

    Commit

--- supervised_learning/decision_tree/0-build_decision_tree.py
+++ supervised_learning/decision_tree/0-build_decision_tree.py
@@ -21,15 +21,17 @@ class Node:
         right_depth = self.right_child.max_depth_below()
         return max(left_depth, right_depth)
 
-def count_nodes_below(self, only_leaves=False):
-    """Documented"""
-    left_count = self.left_child.count_nodes_below(only_leaves) if self.left_child else 0
-    right_count = self.right_child.count_nodes_below(only_leaves) if self.right_child else 0
+    def count_nodes_below(self, only_leaves=False):
+        """Documented"""
+        left_count = self.left_child.count_nodes_below(only_leaves) \
+            if self.left_child else 0
+        right_count = self.right_child.count_nodes_below(only_leaves) \
+            if self.right_child else 0
 
-    if only_leaves:
-        return left_count + right_count
-    else:
-        return 1 + left_count + right_count
+        if only_leaves:
+            return left_count + right_count
+        else:
+            return 1 + left_count + right_count
 
 
 class Leaf(Node):
