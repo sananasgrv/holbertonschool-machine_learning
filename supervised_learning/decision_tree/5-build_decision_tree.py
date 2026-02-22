4234d17fa81f05ebf37bfdd835659ccde1ed67f4
Sanan <sanan.asgarov2023@khazar.org>
2026-02-22 12:47:18 +0400

    COmmit

---
+++ supervised_learning/decision_tree/5-build_decision_tree.py
@@ -0,0 +1,186 @@
+#!/usr/bin/env python3
+"""Documented"""
+
+import numpy as np
+
+
+class Node:
+    """Documented"""
+
+    def __init__(self, feature=None, threshold=None, left_child=None,
+                 right_child=None, is_root=False, depth=0):
+        """Documented"""
+        self.feature = feature
+        self.threshold = threshold
+        self.left_child = left_child
+        self.right_child = right_child
+        self.is_leaf = False
+        self.is_root = is_root
+        self.sub_population = None
+        self.depth = depth
+
+    def max_depth_below(self):
+        """Documented"""
+        return max(self.left_child.max_depth_below(),
+                   self.right_child.max_depth_below())
+
+    def count_nodes_below(self, only_leaves=False):
+        """Documented"""
+        return (self.left_child.count_nodes_below(only_leaves=only_leaves) +
+                self.right_child.count_nodes_below(only_leaves=only_leaves) +
+                (0 if only_leaves else 1))
+
+    def left_child_add_prefix(self, text):
+        """Documented"""
+        lines = text.split("\n")
+        new_text = "    +---> " + lines[0] + "\n"
+        for x in lines[1:]:
+            if x:
+                new_text += ("    |  " + x) + "\n"
+        return new_text
+
+    def right_child_add_prefix(self, text):
+        """Documented"""
+        lines = text.split("\n")
+        new_text = "    +---> " + lines[0] + "\n"
+        for x in lines[1:]:
+            if x:
+                new_text += ("       " + x) + "\n"
+        return new_text
+
+    def __str__(self):
+        """Documented"""
+        if self.is_root:
+            out = (f"root [feature={self.feature}, "
+                   f"threshold={self.threshold}]\n")
+        else:
+            out = (f"node [feature={self.feature}, "
+                   f"threshold={self.threshold}]\n")
+
+        if self.left_child:
+            out += self.left_child_add_prefix(str(self.left_child))
+        if self.right_child:
+            out += self.right_child_add_prefix(str(self.right_child))
+        return out
+
+    def get_leaves_below(self):
+        """Documented"""
+        leaves = []
+        if self.left_child:
+            leaves.extend(self.left_child.get_leaves_below())
+        if self.right_child:
+            leaves.extend(self.right_child.get_leaves_below())
+
+        return leaves
+
+    def update_bounds_below(self):
+        """Documented"""
+        if self.is_root:
+            self.upper = {0: np.inf}
+            self.lower = {0: -1*np.inf}
+
+        for child in [self.left_child, self.right_child]:
+            if child is None:
+                continue
+
+            child.upper = self.upper.copy()
+            child.lower = self.lower.copy()
+
+            f = self.feature
+            t = self.threshold
+
+            if child == self.left_child:
+                child.lower[f] = t
+            else:
+                child.upper[f] = t
+
+        for child in [self.left_child, self.right_child]:
+            child.update_bounds_below()
+
+    def update_indicator(self):
+        """Documented"""
+
+        def is_large_enough(x):
+            """Documented"""
+            if not self.upper:
+                return np.ones(x.shape[0], dtype=bool)
+            return np.all(np.array([x[:, key] >= self.lower[key] for key in self.lower.keys()]), axis=0)
+
+        def is_small_enough(x):
+            """Documented"""
+            if not self.upper:
+                return np.ones(x.shape[0], dtype=bool)
+            return np.all(np.array([x[:, key] <= self.upper[key]for key in self.upper.keys()]), axis=0)
+
+
+        self.indicator = lambda x: np.all(np.array([is_large_enough(x), is_small_enough(x)]), axis=0)
+
+
+class Leaf(Node):
+    """Documented"""
+
+    def __init__(self, value, depth=None):
+        """Documented"""
+        super().__init__()
+        self.value = value
+        self.is_leaf = True
+        self.depth = depth
+
+    def max_depth_below(self):
+        """Documented"""
+        return self.depth
+
+    def count_nodes_below(self, only_leaves=False):
+        """Documented"""
+        return 1
+
+    def __str__(self):
+        """Documented"""
+        return "-> leaf [value={}]".format(self.value)
+
+    def get_leaves_below(self):
+        """Documented"""
+        return [self]
+
+    def update_bounds_below(self):
+        """Documented"""
+        pass
+
+
+class Decision_Tree():
+    """Documented"""
+
+    def __init__(self, max_depth=10, min_pop=1, seed=0,
+                 split_criterion="random", root=None):
+        """Documented"""
+        self.rng = np.random.default_rng(seed)
+        if root:
+            self.root = root
+        else:
+            self.root = Node(is_root=True)
+        self.explanatory = None
+        self.target = None
+        self.max_depth = max_depth
+        self.min_pop = min_pop
+        self.split_criterion = split_criterion
+        self.predict = None
+
+    def depth(self):
+        """Documented"""
+        return self.root.max_depth_below()
+
+    def count_nodes(self, only_leaves=False):
+        """Documented"""
+        return self.root.count_nodes_below(only_leaves=only_leaves)
+
+    def __str__(self):
+        """Documented"""
+        return self.root.__str__()
+
+    def get_leaves(self):
+        """Documented"""
+        return self.root.get_leaves_below()
+
+    def update_bounds(self):
+        """Documented"""
+        self.root.update_bounds_below()
