8b10d90448a234f28e895c46756cefe8fa2ef156
sananasgrv <sanan.asgarov2023@khazar.org>
2026-03-04 16:28:57 +0400

    Commit

--- supervised_learning/classification/0-neuron.py
+++ supervised_learning/classification/0-neuron.py
@@ -6,7 +6,7 @@ import numpy as np
 class Neuron:
     """Neuron"""
     def __init__(self, nx, b=0, A=0):
-        if not type(nx) == int:
+        if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
