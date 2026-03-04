0a34b58f749d69982a776683c8c3e09d78303de1
sananasgrv <sanan.asgarov2023@khazar.org>
2026-03-04 16:44:57 +0400

    Commit

---
+++ supervised_learning/classification/1-neuron.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+""""Documented"""
+import numpy as np
+
+
+class Neuron:
+    """Neuron"""
+
+    def __init__(self, nx, b=0, A=0):
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    def W(self):
+        """Documented"""
+        return self.__W
+
+    def b(self):
+        """Documented"""
+        return self.__b
+
+    def A(self):
+        """Documented"""
+        return self.__A
