096bbcec74fa1d59b9a6ad552c7419265171a00e
sananasgrv <sanan.asgarov2023@khazar.org>
2026-03-06 19:25:14 +0400

    Commit

--- supervised_learning/classification/8-neural_network.py
+++ supervised_learning/classification/8-neural_network.py
@@ -7,18 +7,22 @@ class NeuralNetwork:
     """Documented"""
 
     def __init__(self, nx, nodes):
+        """Documented"""
+
         if not isinstance(nx, int):
             raise TypeError('nx must be an integer')
         if nx < 1:
             raise ValueError('nx must be a positive integer')
+
         if not isinstance(nodes, int):
             raise TypeError('nodes must be an integer')
         if nodes < 1:
-            raise ValueError('nodes must be a positive integerr')
+            raise ValueError('nodes must be a positive integer')
 
         self.W1 = np.random.randn(nodes, nx)
-        self.W2 = np.random.randn(1, nodes)
-        self.b1 = np.zeros(nodes, 1)
-        self.b2 = 0
+        self.b1 = np.zeros((nodes, 1))
         self.A1 = 0
+
+        self.W2 = np.random.randn(1, nodes)
+        self.b2 = 0
         self.A2 = 0
