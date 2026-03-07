222150646d3582f261064d4c735713860cbbe99d
sananasgrv <sanan.asgarov2023@khazar.org>
2026-03-07 21:53:59 +0400

    Commit

--- supervised_learning/classification/28-deep_neural_network.py
+++ supervised_learning/classification/28-deep_neural_network.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Deep Neural Network for multiclass classification"""
+"""Deep Neural Network for multiclass classification with activation option"""
 import numpy as np
 import pickle
 import os
@@ -8,8 +8,8 @@ import os
 class DeepNeuralNetwork:
     """Defines a deep neural network"""
 
-    def __init__(self, nx, layers):
-        """class constructor"""
+    def __init__(self, nx, layers, activation='sig'):
+        """class constructor with activation function"""
         if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
         if nx < 1:
@@ -17,44 +17,44 @@ class DeepNeuralNetwork:
 
         if not isinstance(layers, list) or len(layers) == 0:
             raise TypeError("layers must be a list of positive integers")
-
         if not all(isinstance(x, int) and x > 0 for x in layers):
             raise TypeError("layers must be a list of positive integers")
+
+        if activation not in ('sig', 'tanh'):
+            raise ValueError("activation must be 'sig' or 'tanh'")
 
         self.__L = len(layers)
         self.__cache = {}
         self.__weights = {}
+        self.__activation = activation
 
         for i in range(1, self.__L + 1):
             prev = nx if i == 1 else layers[i - 2]
-
-            self.__weights["W" + str(i)] = (
-                np.random.randn(layers[i - 1], prev) * np.sqrt(2 / prev)
-            )
-
+            self.__weights["W" + str(i)] = np.random.randn(layers[i - 1], prev) * np.sqrt(2 / prev)
             self.__weights["b" + str(i)] = np.zeros((layers[i - 1], 1))
 
     @property
     def L(self):
-        """number of layers"""
         return self.__L
 
     @property
     def cache(self):
-        """cache dictionary"""
         return self.__cache
 
     @property
     def weights(self):
-        """weights dictionary"""
         return self.__weights
 
+    @property
+    def activation(self):
+        """getter for activation"""
+        return self.__activation
+
     def forward_prop(self, X):
-        """forward propagation"""
+        """forward propagation with activation"""
         self.__cache["A0"] = X
 
         for i in range(1, self.__L + 1):
-
             A_prev = self.__cache["A" + str(i - 1)]
             W = self.__weights["W" + str(i)]
             b = self.__weights["b" + str(i)]
@@ -62,10 +62,15 @@ class DeepNeuralNetwork:
             Z = np.matmul(W, A_prev) + b
 
             if i == self.__L:
+                # softmax for output layer
                 expZ = np.exp(Z - np.max(Z, axis=0, keepdims=True))
                 A = expZ / np.sum(expZ, axis=0, keepdims=True)
             else:
-                A = 1 / (1 + np.exp(-Z))
+                # hidden layers activation
+                if self.__activation == 'sig':
+                    A = 1 / (1 + np.exp(-Z))
+                else:  # tanh
+                    A = np.tanh(Z)
 
             self.__cache["A" + str(i)] = A
 
@@ -74,27 +79,24 @@ class DeepNeuralNetwork:
     def cost(self, Y, A):
         """categorical cross-entropy cost"""
         m = Y.shape[1]
-        cost = -np.sum(Y * np.log(A)) / m
+        cost = -np.sum(Y * np.log(A + 1e-8)) / m
         return cost
 
     def evaluate(self, X, Y):
         """evaluate predictions"""
         A, _ = self.forward_prop(X)
         cost = self.cost(Y, A)
-
         prediction = np.zeros_like(A)
         prediction[np.argmax(A, axis=0), np.arange(A.shape[1])] = 1
-
         return prediction, cost
 
     def gradient_descent(self, Y, cache, alpha=0.05):
-        """performs gradient descent"""
+        """performs gradient descent with activation"""
         m = Y.shape[1]
         A_curr = cache["A" + str(self.__L)]
         dz = A_curr - Y
 
         for i in range(self.__L, 0, -1):
-
             A_prev = cache["A" + str(i - 1)]
             W_key = "W" + str(i)
             b_key = "b" + str(i)
@@ -104,7 +106,11 @@ class DeepNeuralNetwork:
             db = np.sum(dz, axis=1, keepdims=True) / m
 
             if i > 1:
-                dz = np.matmul(W.T, dz) * (A_prev * (1 - A_prev))
+                A_prev_activation = cache["A" + str(i - 1)]
+                if self.__activation == 'sig':
+                    dz = np.matmul(W.T, dz) * (A_prev_activation * (1 - A_prev_activation))
+                else:  # tanh
+                    dz = np.matmul(W.T, dz) * (1 - A_prev_activation ** 2)
 
             self.__weights[W_key] -= alpha * dw
             self.__weights[b_key] -= alpha * db
@@ -112,16 +118,12 @@ class DeepNeuralNetwork:
     def train(self, X, Y, iterations=5000, alpha=0.05,
               verbose=True, graph=True, step=100):
         """train the network"""
-
         if not isinstance(iterations, int):
             raise TypeError("iterations must be an integer")
-
         if iterations <= 0:
             raise ValueError("iterations must be a positive integer")
-
         if not isinstance(alpha, float):
             raise TypeError("alpha must be a float")
-
         if alpha <= 0:
             raise ValueError("alpha must be positive")
 
@@ -129,15 +131,12 @@ class DeepNeuralNetwork:
         steps = []
 
         for i in range(iterations + 1):
-
             A, cache = self.forward_prop(X)
             cost = self.cost(Y, A)
 
             if i % step == 0 or i == iterations:
-
                 if verbose:
                     print(f"Cost after {i} iterations: {cost}")
-
                 if graph:
                     costs.append(cost)
                     steps.append(i)
@@ -159,7 +158,6 @@ class DeepNeuralNetwork:
         """save model"""
         if not filename.endswith(".pkl"):
             filename += ".pkl"
-
         with open(filename, "wb") as f:
             pickle.dump(self, f)
 
@@ -168,6 +166,5 @@ class DeepNeuralNetwork:
         """load model"""
         if not os.path.exists(filename):
             return None
-
         with open(filename, "rb") as f:
             return pickle.load(f)
