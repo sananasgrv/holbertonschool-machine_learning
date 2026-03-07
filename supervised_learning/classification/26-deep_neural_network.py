10746871eb3825ee151e3fbc3fc4211cba3ad43e
sananasgrv <sanan.asgarov2023@khazar.org>
2026-03-07 21:14:08 +0400

    Commit

---
+++ supervised_learning/classification/26-deep_neural_network.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""Documented"""
+import pickle
+import os
+
+
+def save(self, filename):
+    """Obyekti pickle formatında fayla yadda saxlayır."""
+    if not filename.endswith('.pkl'):
+        filename += '.pkl'
+
+    with open(filename, 'wb') as f:
+        pickle.dump(self, f)
+
+
+@staticmethod
+def load(filename):
+    """Fayldan pickle formatlı DeepNeuralNetwork obyektini yükləyir."""
+    if not os.path.exists(filename):
+        return None
+
+    with open(filename, 'rb') as f:
+        return pickle.load(f)
