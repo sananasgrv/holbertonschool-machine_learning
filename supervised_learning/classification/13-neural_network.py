a97f82cc8a7c5a3886cb45cf8ac259133a8ae164
sananasgrv <sanan.asgarov2023@khazar.org>
2026-03-07 15:40:15 +0400

    Commit

---
+++ supervised_learning/classification/13-neural_network.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python3
+""""Documented"""
+import numpy as np
+
+
+class NeuralNetwork:
+    """Documented"""
+
+    def __init__(self, nx, nodes):
+        """Documented"""
+
+        if not isinstance(nx, int):
+            raise TypeError('nx must be an integer')
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')
+
+        if not isinstance(nodes, int):
+            raise TypeError('nodes must be an integer')
+        if nodes < 1:
+            raise ValueError('nodes must be a positive integer')
+
+        self.__W1 = np.random.randn(nodes, nx)
+        self.__b1 = np.zeros((nodes, 1))
+        self.__A1 = 0
+
+        self.__W2 = np.random.randn(1, nodes)
+        self.__b2 = 0
+        self.__A2 = 0
+
+    @property
+    def W1(self):
+        """Documented"""
+        return self.__W1
+
+    @property
+    def b1(self):
+        """Documented"""
+        return self.__b1
+
+    @property
+    def A1(self):
+        """Documented"""
+        return self.__A1
+
+    @property
+    def W2(self):
+        """Documented"""
+        return self.__W2
+
+    @property
+    def b2(self):
+        """Documented"""
+        return self.__b2
+
+    @property
+    def A2(self):
+        """Documented"""
+        return self.__A2
+
+    def forward_prop(self, X):
+        """Documented"""
+        Z1 = np.dot(self.__W1, X) + self.__b1
+        self.__A1 = 1 / (1 + np.exp(-Z1))
+
+        Z2 = np.dot(self.__W2, self.__A1) + self.__b2
+        self.__A2 = 1 / (1 + np.exp(-Z2))
+
+        return self.__A1, self.__A2
+
+    def cost(self, Y, A):
+        """Documented"""
+        loss = Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A)
+        cost = -np.sum(loss) / Y.shape[1]
+        return cost
+
+    def evaluate(self, X, Y):
+        """Documented"""
+        _, A2 = self.forward_prop(X)
+        cost = self.cost(Y, A2)
+        predict = np.where(A2 >= 0.5, 1, 0).astype(int)
+        return predict, cost
+
+    def gradient_descent(self, X, Y, A1, A2, alpha=0.05):
+        """Documented"""
+        m = Y.shape[1]
+        dz2 = A2 - Y
+        dw2 = np.dot(dz2, A1.T) / m
+        db2 = np.sum(dz2, axis=1, keepdims=True) / m
+
+        dz1 = np.dot(self.__W2.T, dz2) * (A1 * (1 - A1))
+        dw1 = np.dot(dz1, X.T) / m
+        db1 = np.sum(dz1, axis=1, keepdims=True) / m
+
+        self.__W2 -= alpha * dw2
+        self.__b2 -= alpha * db2
+        self.__W1 -= alpha * dw1
+        self.__b1 -= alpha * db1
