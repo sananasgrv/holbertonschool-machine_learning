34dd2424ecb59d5299a18a94686f19376bed7a78
sananasgrv <sanan.asgarov2023@khazar.org>
2026-03-07 15:22:40 +0400

    Commit

---
+++ supervised_learning/classification/12-neural_network.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+""""Documented"""
+import numpy as np
+import self
+
+
+class NeuralNetwork:
+    """Documented"""
+
+    def __init__(self, nx, nodes):
+        """Documented"""
+
+        if not isinstance(nx, int):
+            raise TypeError('nx must be an integer')
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')
+
+        if not isinstance(nodes, int):
+            raise TypeError('nodes must be an integer')
+        if nodes < 1:
+            raise ValueError('nodes must be a positive integer')
+
+        self.__W1 = np.random.randn(nodes, nx)
+        self.__b1 = np.zeros((nodes, 1))
+        self.__A1 = 0
+
+        self.__W2 = np.random.randn(1, nodes)
+        self.__b2 = 0
+        self.__A2 = 0
+
+    @property
+    def W1(self):
+        """Documented"""
+        return self.__W1
+
+    @property
+    def b1(self):
+        """Documented"""
+        return self.__b1
+
+    @property
+    def A1(self):
+        """Documented"""
+        return self.__A1
+
+    @property
+    def W2(self):
+        """Documented"""
+        return self.__W2
+
+    @property
+    def b2(self):
+        """Documented"""
+        return self.__b2
+
+    @property
+    def A2(self):
+        """Documented"""
+        return self.__A2
+
+    def forward_prop(self, X):
+        """Documented"""
+        Z1 = np.dot(self.__W1, X) + self.__b1
+        self.__A1 = 1 / (1 + np.exp(-Z1))
+
+        Z2 = np.dot(self.__W2, self.__A1) + self.__b2
+        self.__A2 = 1 / (1 + np.exp(-Z2))
+
+        return self.__A1, self.__A2
+
+    def cost(self, Y, A):
+        """Documented"""
+        loss = Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A)
+        cost = -np.sum(loss) / Y.shape[1]
+        return cost
+
+    def evaluate(self, X, Y):
+        """Documented"""
+        _,A2 = self.forward_prop(X)
+        cost = self.cost(Y, A2)
+        predict = np.where(A2 >= 0.5, 1, 0).astype(int)
+        return predict, cost
+
