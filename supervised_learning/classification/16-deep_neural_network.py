b0867e6212fe18e371569d8a2bcbf6db4747f0f9
sananasgrv <sanan.asgarov2023@khazar.org>
2026-03-07 16:56:28 +0400

    Commit

---
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+""""Documented"""
+import numpy as np
+
+
+class DeepNeuralNetwork:
+    """Documented"""
+    def __init__(self,nx, layers):
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+
+        if not isinstance(layers, list) or len(layers) < 1:
+            raise TypeError("layers must be a list of positive integerss")
+        for i in layers:
+            if not isinstance(i, int):
+                raise TypeError("layers must be a list of positive integersr")
+
+        self.L = len(layers)
+        self.cache = {}
+        self.weights = {}
+
+        for l in range(1, self.L+1):
+            prev_layer_size = nx if l == 1 else layers[l - 2]
+            self.weights['W' + str(l)] = np.random.randn(layers[l - 1], prev_layer_size) * \
+                                         np.sqrt(2 / prev_layer_size)
+            self.weights['b' + str(l)] = np.zeros((layers[l - 1], 1))
