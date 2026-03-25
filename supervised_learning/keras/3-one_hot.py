39431aeef7185aeb32758f4b418fb46a40d26f72
sananasgrv <sanan.asgarov2025@khazar.org>
2026-03-25 15:35:50 +0400

    Commit

--- supervised_learning/keras/3-one_hot.py
+++ supervised_learning/keras/3-one_hot.py
@@ -2,7 +2,6 @@
 """Documented"""
 import tensorflow.keras as K
 
-
 def optimize_model(network, alpha, beta1, beta2):
     """
     Sets up Adam optimization for a keras model
@@ -16,15 +15,3 @@ def optimize_model(network, alpha, beta1, beta2):
     Returns:
     None
     """
-
-    optimizer = K.optimizers.Adam(
-        learning_rate=alpha,
-        beta_1=beta1,
-        beta_2=beta2
-    )
-
-    network.compile(
-        optimizer=optimizer,
-        loss='categorical_crossentropy',
-        metrics=['accuracy']
-    )
