2c27b103d32451dee3cee5fa3daf89d007c7e152
sananasgrv <sanan.asgarov2025@khazar.org>
2026-03-25 15:08:28 +0400

    Commit

---
+++ supervised_learning/keras/1-input.py
@@ -0,0 +1,34 @@
+import tensorflow.keras as K
+
+
+def build_model(nx, layers, activations, lambtha, keep_prob):
+    """
+    Documented
+    """
+
+    # First layer (this creates the input tensor implicitly)
+    x = K.layers.Dense(
+        layers[0],
+        activation=activations[0],
+        kernel_regularizer=K.regularizers.l2(lambtha),
+        input_shape=(nx,)
+    )()
+
+    # Keep reference to input
+    inputs = x._keras_history.layer.input
+
+    # Hidden layers
+    for i in range(1, len(layers)):
+        # Apply dropout before each new layer
+        x = K.layers.Dropout(1 - keep_prob)(x)
+
+        x = K.layers.Dense(
+            layers[i],
+            activation=activations[i],
+            kernel_regularizer=K.regularizers.l2(lambtha)
+        )(x)
+
+    # Build model
+    model = K.Model(inputs=inputs, outputs=x)
+
+    return model
