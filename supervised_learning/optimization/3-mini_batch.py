f8f9c77f799cb5b3982200f4fdfac09a0d36c91d
sananasgrv <sanan.asgarov2025@khazar.org>
2026-03-27 16:33:20 +0400

    Commit

--- supervised_learning/optimization/3-mini_batch.py
+++ supervised_learning/optimization/3-mini_batch.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 """Documented"""
-import tensorflow as tf
 shuffle_data = __import__('2-shuffle_data').shuffle_data
 
 
@@ -22,7 +21,11 @@ def create_mini_batches(X, Y, batch_size):
     """
 
     X, Y = shuffle_data(X, Y)
-    dataset = tf.data.Dataset.from_tensor_slices((X, Y))
-    dataset = dataset.shuffle(buffer_size=1000).batch(batch_size)
+    mini_batches=[]
 
-    return dataset
+    for i in range(0, X.shape[0], batch_size):
+        X_batch = X[i:i+batch_size]
+        Y_batch = X[i:i+batch_size]
+        mini_batches.append(X_batch, Y_batch)
+
+    return mini_batches
