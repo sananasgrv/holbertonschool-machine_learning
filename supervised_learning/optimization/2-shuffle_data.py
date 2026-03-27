967ff982b67507e40e964c44a17f0bb129630328
sananasgrv <sanan.asgarov2025@khazar.org>
2026-03-27 15:49:27 +0400

    Commit

---
+++ supervised_learning/optimization/2-shuffle_data.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""Documented"""
+import numpy as np
+
+
+def shuffle_data(X, Y):
+    """
+    X is the first numpy.ndarray of shape (m, nx) to shuffle
+
+        m is the number of data points
+        nx is the number of features in X
+
+    Y is the second numpy.ndarray of shape (m, ny) to shuffle
+
+        m is the same number of data points as in X
+        ny is the number of features in Y
+
+    Returns: the shuffled X and Y matrices
+    """
+
+    return np.random.permutation(X), np.random.permutation(Y)
