ae39b391efdbf7820957bf02bf1a70c4c4fd5164
sananasgrv <sanan.asgarov2025@khazar.org>
2026-03-27 15:24:17 +0400

    Commit

---
+++ supervised_learning/optimization/0-norm_constants.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Documented"""
+import numpy as np
+
+def normalization_constants(X):
+    """
+    X is the numpy.ndarray of shape (m, nx) to normalize
+    m is the number of data points
+    nx is the number of features
+    Returns: the mean and standard deviation of each feature, respectively
+    """
+
+    return  np.mean(X, axis=0), np.std(X, axis=0)
