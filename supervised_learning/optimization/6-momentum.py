2dcebee5892a64922d86f5165fd41843d4bd4222
sananasgrv <sanan.asgarov2025@khazar.org>
2026-03-27 17:12:18 +0400

    Commit

--- supervised_learning/optimization/6-momentum.py
+++ supervised_learning/optimization/6-momentum.py
@@ -2,20 +2,17 @@
 """Documented"""
 
 
-def update_variables_momentum(alpha, beta1, var, grad, v):
+def create_momentum_op(alpha, beta1):
     """
-    that updates a variable using the gradient descent
-    with momentum optimization algorithm:
+    that sets up the gradient descent with momentum optimization algorithm in TensorFlow:
 
-    alpha is the learning rate
-    beta1 is the momentum weight
-    var is a numpy.ndarray containing the variable to be updated
-    grad is a numpy.ndarray containing the gradient of var
-    v is the previous first moment of var
-    Returns: the updated variable and the new moment, respectively
+    alpha is the learning rate.
+    beta1 is the momentum weight.
+    Returns: optimizer
+
     """
-    v_new = beta1 * v + (1 - beta1) * grad
-
-    var_updated = var - alpha * v_new
-
-    return var_updated, v_new
+    optimizer = tf.keras.optimizers.SGD(
+        learning_rate=alpha,
+        momentum=beta1
+    )
+    return optimizer
