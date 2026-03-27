1239665c1bf8c3207f86549a87727a43f56f4dbd
sananasgrv <sanan.asgarov2025@khazar.org>
2026-03-27 15:41:35 +0400

    Commit

---
+++ supervised_learning/optimization/1-normalize.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Documented"""
+import numpy as np
+
+def normalize(X, m, s):
+    """
+    X is the numpy.ndarray of shape (d, nx) to normalize
+        d is the number of data points
+        nx is the number of features
+    m is a numpy.ndarray of shape (nx,) that contains the mean of all features of X
+    s is a numpy.ndarray of shape (nx,) that contains the standard deviation of all features of X
+    Returns: The normalized X matrix (z= (x - m) / standart deviation)
+    """
+
+    return (X - m) / s
