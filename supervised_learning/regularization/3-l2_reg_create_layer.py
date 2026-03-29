b451954261e041b36df938cd894e47bdadd54ff0
sananasgrv <sanan.asgarov2025@khazar.org>
2026-03-29 15:31:31 +0400

    Commit

--- supervised_learning/regularization/3-l2_reg_create_layer.py
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -1,15 +1,26 @@
 #!/usr/bin/env python3
-"""Documented"""
+"""Module that creates a neural network layer with L2 regularization."""
+
 import tensorflow as tf
 
 
 def l2_reg_create_layer(prev, n, activation, lambtha):
-    """Creates a layer with L2 regularization"""
+    """Creates a neural network layer with L2 regularization.
 
+    Args:
+        prev: tensor containing the output of the previous layer.
+        n: the number of nodes the new layer should contain.
+        activation: the activation function for the layer.
+        lambtha: the L2 regularization parameter.
+
+    Returns:
+        The output of the new layer.
+    """
+    regularizer = tf.keras.regularizers.L2(lambtha * 2)
     layer = tf.keras.layers.Dense(
         units=n,
         activation=activation,
-        kernel_regularizer=tf.keras.regularizers.l2(lambtha)
-    )(prev)
-
-    return layer
+        kernel_regularizer=regularizer,
+        kernel_initializer='glorot_uniform'
+    )
+    return layer(prev)
