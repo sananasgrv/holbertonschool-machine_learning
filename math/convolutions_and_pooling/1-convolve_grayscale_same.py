713ade1d9048829defcc4b73fc6a4bac65046c00
sananasgrv <sanan.asgarov2025@khazar.org>
2026-03-29 17:00:52 +0400

    Commit

--- math/convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -17,25 +17,16 @@ def convolve_grayscale_same(images, kernel):
     m, h, w = images.shape
     kh, kw = kernel.shape
 
-    # Compute padding for "same" convolution
-    pad_h = (kh - 1) // 2
-    pad_w = (kw - 1) // 2
+    ph = kh // 2
+    pw = kw // 2
 
-    # Pad images with zeros
-    images_padded = np.pad(
-        images,
-        pad_width=((0, 0), (pad_h, kh - pad_h - 1), (pad_w, kw - pad_w - 1)),
-        mode='constant',
-        constant_values=0
-    )
-
-    # Output shape is same as original image
+    padded = np.pad(images, ((0, 0), (ph, ph), (pw, pw)), mode='constant')
     output = np.zeros((m, h, w))
 
-    # Convolution using 2 loops
     for i in range(h):
         for j in range(w):
-            current_slice = images_padded[:, i:i+kh, j:j+kw]
-            output[:, i, j] = np.sum(current_slice * kernel, axis=(1, 2))
-
+            output[:, i, j] = np.sum(
+                padded[:, i:i + kh, j:j + kw] * kernel,
+                axis=(1, 2)
+            )
     return output
