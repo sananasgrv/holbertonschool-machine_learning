c74ba6ef702c1596c09d5d11caf357e2f10bfea0
sananasgrv <sanan.asgarov2025@khazar.org>
2026-03-29 15:37:09 +0400

    Commit

--- supervised_learning/regularization/5-dropout_gradient_descent.py
+++ supervised_learning/regularization/5-dropout_gradient_descent.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 """Module that updates weights using gradient descent with Dropout."""
-
 import numpy as np
 
 
@@ -8,29 +7,29 @@ def dropout_gradient_descent(Y, weights, cache, alpha, keep_prob, L):
     """Updates weights of a neural network with Dropout using gradient descent.
 
     Args:
-        Y: one-hot numpy.ndarray of shape (classes, m) with correct labels.
-        weights: dictionary of the weights and biases of the neural network.
-        cache: dictionary of outputs and dropout masks of each layer.
-        alpha: the learning rate.
-        keep_prob: the probability that a node will be kept.
-        L: the number of layers of the network.
+        Y (numpy.ndarray): one-hot array of shape (classes, m) with correct labels
+        weights (dict): dictionary of weights and biases
+        cache (dict): dictionary of outputs and dropout masks
+        alpha (float): learning rate
+        keep_prob (float): probability that a node will be kept
+        L (int): number of layers
     """
     m = Y.shape[1]
-    dZ = cache['A' + str(L)] - Y
+    dZ = cache['A{}'.format(L)] - Y
     weights_copy = weights.copy()
 
     for i in range(L, 0, -1):
-        A_prev = cache['A' + str(i - 1)]
-        W = weights_copy['W' + str(i)]
+        A_prev = cache['A{}'.format(i - 1)]
+        W = weights_copy['W{}'.format(i)]
 
         dW = np.matmul(dZ, A_prev.T) / m
         db = np.sum(dZ, axis=1, keepdims=True) / m
 
         if i > 1:
             dA = np.matmul(W.T, dZ)
-            dA = dA * cache['D' + str(i - 1)] / keep_prob
-            dZ = dA * (1 - cache['A' + str(i - 1)] ** 2)
+            dA *= cache['D{}'.format(i - 1)]
+            dA /= keep_prob
+            dZ = dA * (1 - cache['A{}'.format(i - 1)] ** 2)
 
-        weights['W' + str(i)] -= alpha * dW
-        weights['b' + str(i)] -= alpha * db
-        
+        weights['W{}'.format(i)] -= alpha * dW
+        weights['b{}'.format(i)] -= alpha * db
