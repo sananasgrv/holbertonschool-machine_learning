61d1d98cd1b4e466e226d17eec08b79b0412dc6c
sananasgrv <sanan.asgarov2025@khazar.org>
2026-03-29 16:59:27 +0400

    Commit

--- math/convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -1,33 +1,41 @@
 #!/usr/bin/env python3
-"""Documented"""
+"""Same convolution on grayscale images"""
+
 import numpy as np
 
-
-def convolve_grayscale_valid(images, kernel):
+def convolve_grayscale_same(images, kernel):
     """
-    that performs a valid convolution on grayscale images:
-    images is a numpy.ndarray with shape (m, h, w) containing multiple grayscale images
-        m is the number of images
-        h is the height in pixels of the images
-        w is the width in pixels of the images
-    kernel is a numpy.ndarray with shape (kh, kw) containing the kernel for the convolution
-        kh is the height of the kernel
-        kw is the width of the kernel
-    You are only allowed to use two for loops; any other loops of any kind are not allowed
-    Returns: a numpy.ndarray containing the convolved images
+    Performs a same convolution on grayscale images
+
+    Args:
+        images: numpy.ndarray of shape (m, h, w)
+        kernel: numpy.ndarray of shape (kh, kw)
+
+    Returns:
+        numpy.ndarray containing the convolved images
     """
-        m, h, w = images.shape
-        kh, kw = kernel.shape
+    m, h, w = images.shape
+    kh, kw = kernel.shape
 
-        output_h = h - kh + 1
-        output_w = w - kw + 1
+    # Compute padding for "same" convolution
+    pad_h = (kh - 1) // 2
+    pad_w = (kw - 1) // 2
 
-        output = np.zeros((m, output_h, output_w))
+    # Pad images with zeros
+    images_padded = np.pad(
+        images,
+        pad_width=((0, 0), (pad_h, kh - pad_h - 1), (pad_w, kw - pad_w - 1)),
+        mode='constant',
+        constant_values=0
+    )
 
-        for i in range(output_h):
-            for j in range(output_w):
-                current_slice = images[:, i:i + kh, j:j + kw]
+    # Output shape is same as original image
+    output = np.zeros((m, h, w))
 
-                output[:, i, j] = np.sum(current_slice * kernel, axis=(1, 2))
+    # Convolution using 2 loops
+    for i in range(h):
+        for j in range(w):
+            current_slice = images_padded[:, i:i+kh, j:j+kw]
+            output[:, i, j] = np.sum(current_slice * kernel, axis=(1, 2))
 
-        return output
+    return output
