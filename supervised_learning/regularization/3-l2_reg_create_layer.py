9b2ce384eb521f2187a7e51688617fbd3648c4a2
sananasgrv <sanan.asgarov2025@khazar.org>
2026-03-29 15:39:48 +0400

    Commit

--- supervised_learning/regularization/3-l2_reg_create_layer.py
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -6,11 +6,13 @@ import tensorflow as tf
 def l2_reg_create_layer(prev, n, activation, lambtha):
     """Creates a neural network layer with L2 regularization."""
 
-    layer = tf.keras.layers.Dense(
+    regularizer = tf.keras.regularizers.l2(lambtha)
+    layer_weight = tf.initializers.VarianceScaling(
+        scale=2.0, mode=("fan_avg"))
+    output_layer = tf.keras.layers.Dense(
         units=n,
         activation=activation,
-        kernel_regularizer=tf.keras.regularizers.l2(lambtha),
-        kernel_initializer='glorot_uniform'
-    )
-
-    return layer(prev)
+        kernel_regularizer=regularizer,
+        kernel_initializer=layer_weight
+    )(prev)
+    return output_layer
