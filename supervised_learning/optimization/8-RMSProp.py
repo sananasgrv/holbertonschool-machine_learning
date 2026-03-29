8adefbf102ff34a688c01fe0c2dbb95a0b746717
sananasgrv <sanan.asgarov2025@khazar.org>
2026-03-29 13:59:05 +0400

    Commit

---
+++ supervised_learning/optimization/8-RMSProp.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Documented"""
+import numpy as np
+
+
+def create_RMSProp_op(alpha, beta2, epsilon):
+    """
+    that sets up the RMSProp optimization algorithm in TensorFlow:
+
+    alpha is the learning rate
+    beta2 is the RMSProp weight (Discounting factor)
+    epsilon is a small number to avoid division by zero
+    Returns: optimizer
+    """
+    return tf.keras.optimizers.RMSprop(
+        learning_rate=alpha,
+        rho=beta2,
+        epsilon=epsilon
+    )
