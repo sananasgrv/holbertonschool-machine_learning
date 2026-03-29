6951f7e65724cf679c76908801b0f3797491e935
sananasgrv <sanan.asgarov2025@khazar.org>
2026-03-29 15:14:49 +0400

    Commit

--- supervised_learning/regularization/3-l2_reg_create_layer.py
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
 """Documented"""
-import tensorflow.keras as K
+import tensorflow as tf
 
 
 def l2_reg_create_layer(prev, n, activation, lambtha):
     """Documented"""
 
-    
+    layer = tf.keras.layer.Dense(
+        units=n,
+        activation=activation,
+        kernel_regularizer = tf.keras.regularizers.l2(lambtha)(prev)
+    )
+    return layer
