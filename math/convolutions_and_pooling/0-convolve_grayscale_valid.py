4df5fa263aabf928393bcf4fcf2d15b4e670d7c1
sananasgrv <sanan.asgarov2025@khazar.org>
2026-03-29 16:58:22 +0400

    Commit

--- math/convolutions_and_pooling/0-convolve_grayscale_valid.py
+++ math/convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -1,33 +1,31 @@
 #!/usr/bin/env python3
-"""Documented"""
+"""VDocumented"""
 import numpy as np
 
 
 def convolve_grayscale_valid(images, kernel):
     """
-    that performs a valid convolution on grayscale images:
-    images is a numpy.ndarray with shape (m, h, w) containing multiple grayscale images
-        m is the number of images
-        h is the height in pixels of the images
-        w is the width in pixels of the images
-    kernel is a numpy.ndarray with shape (kh, kw) containing the kernel for the convolution
-        kh is the height of the kernel
-        kw is the width of the kernel
-    You are only allowed to use two for loops; any other loops of any kind are not allowed
-    Returns: a numpy.ndarray containing the convolved images
+    Performs a valid convolution on grayscale images
+
+    Args:
+        images: numpy.ndarray of shape (m, h, w)
+        kernel: numpy.ndarray of shape (kh, kw)
+
+    Returns:
+        numpy.ndarray containing the convolved images
     """
-        m, h, w = images.shape
-        kh, kw = kernel.shape
+    m, h, w = images.shape
+    kh, kw = kernel.shape
 
-        output_h = h - kh + 1
-        output_w = w - kw + 1
+    output_h = h - kh + 1
+    output_w = w - kw + 1
 
-        output = np.zeros((m, output_h, output_w))
+    output = np.zeros((m, output_h, output_w))
 
-        for i in range(output_h):
-            for j in range(output_w):
-                current_slice = images[:, i:i + kh, j:j + kw]
+    for i in range(output_h):
+        for j in range(output_w):
+            current_slice = images[:, i:i+kh, j:j+kw]
 
-                output[:, i, j] = np.sum(current_slice * kernel, axis=(1, 2))
+            output[:, i, j] = np.sum(current_slice * kernel, axis=(1, 2))
 
-        return output
+    return output
