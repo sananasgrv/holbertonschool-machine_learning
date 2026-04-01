865df35cdbf58e7a25d67528f0bbba13b7a3ab8d
sananasgrv <sanan.asgarov2025@khazar.org>
2026-04-01 14:08:05 +0400

    Commit

---
+++ supervised_learning/cnn/0-conv_forward.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+"""Module that performs a convolution on images using multiple kernels."""
+
+import numpy as np
+
+
+def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
+    """
+    that performs forward propagation over a convolutional layer of a neural network:
+
+    A_prev is a numpy.ndarray of shape (m, h_prev, w_prev, c_prev) containing the output of the previous layer
+        m is the number of examples
+        h_prev is the height of the previous layer
+        w_prev is the width of the previous layer
+        c_prev is the number of channels in the previous layer
+    W is a numpy.ndarray of shape (kh, kw, c_prev, c_new) containing the kernels for the convolution
+        kh is the filter height
+        kw is the filter width
+        c_prev is the number of channels in the previous layer
+        c_new is the number of channels in the output
+    b is a numpy.ndarray of shape (1, 1, 1, c_new) containing the biases applied to the convolution
+    activation is an activation function applied to the convolution
+    padding is a string that is either same or valid, indicating the type of padding used
+    stride is a tuple of (sh, sw) containing the strides for the convolution
+        sh is the stride for the height
+        sw is the stride for the width
+    you may import numpy as np
+    Returns: the output of the convolutional layer
+
+    """
+    m, h_prev, w_prev, c_prev = A_prev.shape
+    kh, kw, _, c_new = W.shape
+    sh, sw = stride
+
+    if padding == 'same':
+        ph = max((h_prev - 1) * sh - h_prev + kh, 0) // 2
+        pw = max((w_prev - 1) * sw - w_prev + kw, 0) // 2
+    else:
+        ph, pw = 0, 0
+
+    oh = (h_prev + 2 * ph - kh) // sh + 1
+    ow = (w_prev + 2 * pw - kw) // sw + 1
+
+    padded = np.pad(
+        A_prev, ((0, 0), (ph, ph), (pw, pw), (0, 0)), mode='constant'
+    )
+
+    Z = np.zeros((m, oh, ow, c_new))
+    for i in range(oh):
+        for j in range(ow):
+            for k in range(c_new):
+                Z[:, i, j, k] = np.sum(
+                    padded[:, i * sh:i * sh + kh,
+                    j * sw:j * sw + kw, :] *
+                    W[:, :, :, k],
+                    axis=(1, 2, 3)
+                ) + b[0, 0, 0, k]
+
+    return activation(Z)
