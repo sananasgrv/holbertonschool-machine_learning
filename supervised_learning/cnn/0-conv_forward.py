c295d684cd6092e8e90efed0b296e992c9b7115c
sananasgrv <sanan.asgarov2025@khazar.org>
2026-04-01 14:11:01 +0400

    Commit

--- supervised_learning/cnn/0-conv_forward.py
+++ supervised_learning/cnn/0-conv_forward.py
@@ -6,22 +6,28 @@ import numpy as np
 
 def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
     """
-    that performs forward propagation over a convolutional layer of a neural network:
+    that performs forward propagation over a convolutional
+    layer of a neural network:
 
-    A_prev is a numpy.ndarray of shape (m, h_prev, w_prev, c_prev) containing the output of the previous layer
+    A_prev is a numpy.ndarray of shape (m, h_prev, w_prev, c_prev)
+     containing the output of the previous layer
         m is the number of examples
         h_prev is the height of the previous layer
         w_prev is the width of the previous layer
         c_prev is the number of channels in the previous layer
-    W is a numpy.ndarray of shape (kh, kw, c_prev, c_new) containing the kernels for the convolution
+    W is a numpy.ndarray of shape (kh, kw, c_prev, c_new)
+     containing the kernels for the convolution
         kh is the filter height
         kw is the filter width
         c_prev is the number of channels in the previous layer
         c_new is the number of channels in the output
-    b is a numpy.ndarray of shape (1, 1, 1, c_new) containing the biases applied to the convolution
+    b is a numpy.ndarray of shape (1, 1, 1, c_new)
+    containing the biases applied to the convolution
     activation is an activation function applied to the convolution
-    padding is a string that is either same or valid, indicating the type of padding used
-    stride is a tuple of (sh, sw) containing the strides for the convolution
+    padding is a string that is either same or valid,
+     indicating the type of padding used
+    stride is a tuple of (sh, sw) containing
+    the strides for the convolution
         sh is the stride for the height
         sw is the stride for the width
     you may import numpy as np
