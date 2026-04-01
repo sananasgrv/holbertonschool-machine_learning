0f19766fcefc3c049585a4df77bd4b7cf73c1349
sananasgrv <sanan.asgarov2025@khazar.org>
2026-04-01 14:17:16 +0400

    Commit

--- supervised_learning/cnn/0-conv_forward.py
+++ supervised_learning/cnn/0-conv_forward.py
@@ -1,68 +1,47 @@
 #!/usr/bin/env python3
-"""Module that performs a convolution on images using multiple kernels."""
-
+"""Module that performs forward propagation over a convolutional layer."""
 import numpy as np
 
 
 def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
+    """Performs forward propagation over a convolutional layer.
+
+    Args:
+        A_prev: numpy.ndarray of shape (m, h_prev, w_prev, c_prev).
+        W: numpy.ndarray of shape (kh, kw, c_prev, c_new) with kernels.
+        b: numpy.ndarray of shape (1, 1, 1, c_new) with biases.
+        activation: activation function applied to the convolution.
+        padding: 'same' or 'valid'.
+        stride: tuple of (sh, sw) for height and width strides.
+
+    Returns:
+        The output of the convolutional layer.
     """
-    Performs forward propagation over a convolutional layer
-    of a neural network.
-
-    A_prev: numpy.ndarray of shape (m, h_prev, w_prev, c_prev)
-    W: numpy.ndarray of shape (kh, kw, c_prev, c_new)
-    b: numpy.ndarray of shape (1, 1, 1, c_new)
-    activation: activation function
-    padding: "same" or "valid"
-    stride: tuple (sh, sw)
-
-    Returns: output of the convolutional layer
-    """
-
     m, h_prev, w_prev, c_prev = A_prev.shape
     kh, kw, _, c_new = W.shape
     sh, sw = stride
 
-    # Padding hesablanması
-    if padding == "same":
-        ph = int(np.ceil(((h_prev - 1) * sh + kh - h_prev) / 2))
-        pw = int(np.ceil(((w_prev - 1) * sw + kw - w_prev) / 2))
+    if padding == 'same':
+        ph = max((h_prev - 1) * sh - h_prev + kh, 0) // 2
+        pw = max((w_prev - 1) * sw - w_prev + kw, 0) // 2
     else:
         ph, pw = 0, 0
 
-    # Output ölçüləri
-    oh = int((h_prev + 2 * ph - kh) / sh) + 1
-    ow = int((w_prev + 2 * pw - kw) / sw) + 1
+    oh = (h_prev + 2 * ph - kh) // sh + 1
+    ow = (w_prev + 2 * pw - kw) // sw + 1
 
-    # Padding tətbiqi
-    A_prev_pad = np.pad(
-        A_prev,
-        ((0, 0), (ph, ph), (pw, pw), (0, 0)),
-        mode="constant"
+    padded = np.pad(
+        A_prev, ((0, 0), (ph, ph), (pw, pw), (0, 0)), mode='constant'
     )
 
-    # Output tensor
     Z = np.zeros((m, oh, ow, c_new))
-
     for i in range(oh):
         for j in range(ow):
             for k in range(c_new):
-
-                vert_start = i * sh
-                vert_end = vert_start + kh
-
-                horiz_start = j * sw
-                horiz_end = horiz_start + kw
-
-                slice_prev = A_prev_pad[
-                    :,
-                    vert_start:vert_end,
-                    horiz_start:horiz_end,
-                    :
-                ]
-
                 Z[:, i, j, k] = np.sum(
-                    slice_prev * W[:, :, :, k],
+                    padded[:, i * sh:i * sh + kh,
+                           j * sw:j * sw + kw, :] *
+                    W[:, :, :, k],
                     axis=(1, 2, 3)
                 ) + b[0, 0, 0, k]
 
--- supervised_learning/cnn/1-pool_forward.py
+++ supervised_learning/cnn/1-pool_forward.py
@@ -1,48 +1,26 @@
 #!/usr/bin/env python3
-"""Module that performs forward propagation over a convolutional layer."""
+"""Documented"""
 import numpy as np
 
 
-def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
-    """Performs forward propagation over a convolutional layer.
+def pool_forward(A_prev, kernel_shape, stride=(1, 1), mode='max'):
+    """Documented"""
 
-    Args:
-        A_prev: numpy.ndarray of shape (m, h_prev, w_prev, c_prev).
-        W: numpy.ndarray of shape (kh, kw, c_prev, c_new) with kernels.
-        b: numpy.ndarray of shape (1, 1, 1, c_new) with biases.
-        activation: activation function applied to the convolution.
-        padding: 'same' or 'valid'.
-        stride: tuple of (sh, sw) for height and width strides.
-
-    Returns:
-        The output of the convolutional layer.
-    """
     m, h_prev, w_prev, c_prev = A_prev.shape
-    kh, kw, _, c_new = W.shape
+    kh, kw = kernel_shape
     sh, sw = stride
 
-    if padding == 'same':
-        ph = max((h_prev - 1) * sh - h_prev + kh, 0) // 2
-        pw = max((w_prev - 1) * sw - w_prev + kw, 0) // 2
-    else:
-        ph, pw = 0, 0
+    oh = (h_prev - kh) // sh + 1
+    ow = (w_prev - kw) // sw + 1
 
-    oh = (h_prev + 2 * ph - kh) // sh + 1
-    ow = (w_prev + 2 * pw - kw) // sw + 1
+    A = np.zeros((m, oh, ow, c_prev))
 
-    padded = np.pad(
-        A_prev, ((0, 0), (ph, ph), (pw, pw), (0, 0)), mode='constant'
-    )
-
-    Z = np.zeros((m, oh, ow, c_new))
     for i in range(oh):
         for j in range(ow):
-            for k in range(c_new):
-                Z[:, i, j, k] = np.sum(
-                    padded[:, i * sh:i * sh + kh,
-                           j * sw:j * sw + kw, :] *
-                    W[:, :, :, k],
-                    axis=(1, 2, 3)
-                ) + b[0, 0, 0, k]
+            region = A_prev[:, i * sh:i * sh + kh, j * sw:j * sw + kw, :]
+            if mode == 'max':
+                A[:, i, j, :] = np.max(region, axis=(1, 2))
+            else:
+                A[:, i, j, :] = np.mean(region, axis=(1, 2))
 
-    return activation(Z)
+    return A
