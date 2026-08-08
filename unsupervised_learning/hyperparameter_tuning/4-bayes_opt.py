73cdb9587e74df1af78e088f57e76fe456914220
sananasgrv <sanan.asgarov2023@khazar.org>
2026-08-08 04:34:23 +0400

    Commiting changes

--- unsupervised_learning/hyperparameter_tuning/4-bayes_opt.py
+++ unsupervised_learning/hyperparameter_tuning/4-bayes_opt.py
@@ -16,3 +16,17 @@ class BayesianOptimization:
         self.xsi = xsi
         self.minimize = minimize
         self.X_s = np.linspace(bounds[0], bounds[1], ac_samples).reshape(-1, 1)
+    def acquisition(self):
+        """Calculates the next best sample location"""
+        mu, sigma = self.gp.predict(self.X_s)
+        if self.minimize:
+            mu_sample_opt = np.min(self.gp.Y)
+            imp = mu_sample_opt - mu - self.xsi
+        else:
+            mu_sample_opt = np.max(self.gp.Y)
+            imp = mu - mu_sample_opt - self.xsi
+        Z = imp / sigma
+        ei = imp * norm.cdf(Z) + sigma * norm.pdf(Z)
+        ei[sigma == 0.0] = 0.0
+        X_next = self.X_s[np.argmax(ei)]
+        return X_next, ei
