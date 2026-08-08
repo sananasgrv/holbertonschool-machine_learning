b3d1d3d2129e5f725a7337022e3a19025ac32d08
sananasgrv <sanan.asgarov2023@khazar.org>
2026-08-08 22:44:06 +0400

    Commiting change

--- unsupervised_learning/autoencoders/0-vanilla.py
+++ unsupervised_learning/autoencoders/0-vanilla.py
@@ -1,28 +1,25 @@
 #!/usr/bin/env python3
 """Vanilla Autoencoder implementation."""
-import numpy as np
-import tensorflow as tf
-from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Input, Dense
+import tensorflow.keras as keras
 
 
 def autoencoder(input_dims, hidden_layers, latent_dims):
     """Function that creates a vanilla autoencoder"""
-    input_layer = Input(shape=(input_dims,))
-    x = input_layer
+    input_layer = keras.Input(shape=(input_dims,))
+    encoder = input_layer
 
-    for units in hidden_layers:
-        x = Dense(units, activation='relu')(x)
+    for layer in hidden_layers:
+        encoder = keras.layers.Dense(layer, activation='relu')(encoder)
 
-    latent_layer = Dense(latent_dims, activation='relu')(x)
+    latent_space = keras.layers.Dense(latent_dims, activation='relu')(encoder)
 
-    x = latent_layer
-    for units in reversed(hidden_layers):
-        x = Dense(units, activation='relu')(x)
+    decoder = latent_space
+    for layer in reversed(hidden_layers):
+        decoder = keras.layers.Dense(layer, activation='relu')(decoder)
 
-    output_layer = Dense(input_dims, activation='sigmoid')(x)
+    output_layer = keras.layers.Dense(input_dims, activation='sigmoid')(decoder)
 
-    autoencoder_model = Model(inputs=input_layer, outputs=output_layer)
-    encoder_model = Model(inputs=input_layer, outputs=latent_layer)
+    autoencoder_model = keras.Model(inputs=input_layer, outputs=output_layer)
+    encoder_model = keras.Model(inputs=input_layer, outputs=latent_space)
 
     return autoencoder_model, encoder_model
