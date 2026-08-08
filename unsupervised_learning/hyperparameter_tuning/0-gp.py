735c841aca7e9ce8595ec5868faa46e7c8b856a6
sananasgrv <sanan.asgarov2023@khazar.org>
2026-08-08 04:16:21 +0400

    Commiting changes

--- unsupervised_learning/hyperparameter_tuning/0-gp.py
+++ unsupervised_learning/hyperparameter_tuning/0-gp.py
@@ -13,7 +13,7 @@ def __init__(self, X_init, Y_init, l=1, sigma_f=1):
 
 def kernel(self, X1, X2):
     """Calculates the covariance kernel matrix between two matrices"""
-    sqr = (X1 - X2.T) ** 2
-    k = self.sigma_f ** 2 * np.exp(-sqr / (2 * self.l ** 2))
-    return k
-    
+    sqdist = (X1 - X2.T) ** 2
+    return (self.sigma_f ** 2) * np.exp(
+        -sqdist / (2 * self.l ** 2)
+        )
