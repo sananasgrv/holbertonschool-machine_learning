de6843109312940039cb91bfc70e192088cdd11c
sananasgrv <sanan.asgarov2023@khazar.org>
2026-08-30 15:25:17 +0400

    fixed

--- reinforcement_learning/q_learning/4-play.py
+++ reinforcement_learning/q_learning/4-play.py
@@ -1,20 +1,24 @@
 #!/usr/bin/env python3
-"""Has the trained agent play an episode of FrozenLake."""
+"""Comment"""
 import numpy as np
 
 
 def play(env, Q, max_steps=100):
-    """Has the trained agent play an episode, always exploiting the Q-table."""
+    """Comment"""
+
     state = env.reset()[0]
     rendered_outputs = []
     total_rewards = 0
 
     for step in range(max_steps):
         rendered_outputs.append(env.render())
+
         action = np.argmax(Q[state])
-        new_state, reward, terminated, truncated, info = env.step(action)
+
+        new_state, reward, terminated, truncated, _ = env.step(action)
+
         state = new_state
-        total_rewards = total_rewards + reward
+        total_rewards += reward
 
         if terminated or truncated:
             rendered_outputs.append(env.render())
