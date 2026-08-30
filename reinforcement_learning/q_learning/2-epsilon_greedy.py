41e8356988c0075caf84f391cf8969f6cf233a98
sananasgrv <sanan.asgarov2023@khazar.org>
2026-08-30 15:05:48 +0400

    fixed

---
+++ reinforcement_learning/q_learning/2-epsilon_greedy.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Comment"""
+import numpy as np
+
+
+def epsilon_greedy(Q, state, epsilon):
+    """Comment"""
+    p = np.random.uniform(0, 1)
+    if p < epsilon:
+        action = np.random.randint(0, Q.shape[1])
+    else:
+        action = np.argmax(Q[state])
+    return action
